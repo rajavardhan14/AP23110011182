76b0a27d64d91ffb1a31fac79142e1f4d3a85624
rajavardhan14 <rajavardhan_kasa@srmap.edu.in>
2026-05-02 10:31:39 +0530

    Set up Express server with logging middleware

--- logging middleware/notification_app_be/server.js
+++ logging middleware/notification_app_be/server.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const logger = require('../logging_middleware/logger');
 
+const app = express();
+app.use(express.json());
+app.use(logger);
+
+app.get('/', (req, res) => {
+res.send('Backend running');
+});
+
+app.post('/notify', (req, res) => {
+res.json({ message: req.body.message });
+});
+
+app.listen(3000);
